53c1ec06aa3e60b0af36687517202088d0fb166c
nengkya <nengkya@yahoo.com>
2025-03-24 04:27:14 +0700

    post object_list doesnt appear in browser !post object_list doesnt appear in browser !

--- posts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-#create your models here
-class Post(models.Model):
-    text = models.TextField()
-
-    def __str__(self):
-        return self.text[:50]
--- posts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-from django.views.generic import ListView
-
-#create your views here
-class HomePageView(ListView):
-    model = Post
-    template_name = 'home.html'
-    context_object_name = 'all_posts_list'
