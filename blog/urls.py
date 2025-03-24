ba846f18b24ba01106e41e4c8bd10536acae46cd
nengkya <nengkya@yahoo.com>
2025-03-25 05:07:33 +0700

    Block content detail doesnt render. Why ?

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 #urlspatterns
 #the included URLconf '<module 'blog.urls' from '/home/nengkya/blog_project/blog/urls.py'>' does not appear to have any patterns in it
 #if you see the 'urlpatterns' variable with valid patterns in the file then the issue is probably caused by a circular import
 urlpatterns = [
 
-    path('', BlogListView.as_view(), name = 'home')
+    path('', BlogListView.as_view(), name = 'home'),
+
+    path('post/<int:pk>/', BlogDetailView.as_view(), name = 'post_detail')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+class BlogDetailView(ListView):
+    model = Post
+    template_name = 'post_detail.html'
